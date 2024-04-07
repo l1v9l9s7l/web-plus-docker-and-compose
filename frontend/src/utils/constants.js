34211c0d9990fbaf73fb7e4cc51113196576673d
l1v9l9s7l <l1v9l9s7l@gmail.com>
2024-04-08 00:19:24 +0300

    fix url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://api.kpd-l1.nomoredomainswork.ru/signup";
+export const URL = "http://api.kpd-l1.nomoredomainswork.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
